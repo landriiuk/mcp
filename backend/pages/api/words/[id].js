b87f57ff61168b9e8e7ca1ae1088810c1094905f
landriiuk <andriukluba@gmail.com>
2026-07-07 00:04:51 +0300

    add headers

--- backend/pages/api/words/[id].js
+++ backend/pages/api/words/[id].js
@@ -21,6 +21,14 @@ async function ensureFolderExists(folderName) {
 }
 
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+
   const { id } = req.query
 
   if (!id) {
